c89ea9143bbf0badddf089108ca0d14d87e7b267
ToseebNadaf <toseebnadaf@gmail.com>
2025-03-04 15:54:28 +0530

    Setup http server & added user schema

--- src/server.js
+++ src/server.js
@@ -1,1 +1,21 @@
-console.log("Hello");
+const express = require("express");
+const dotenv = require("dotenv");
+const cors = require("cors");
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Middlewares
+app.use(cors());
+app.use(express.json());
+
+// Test route
+app.get("/", (req, res) => {
+  res.send("E-Commerce API is running!");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
